f318f1b867316e664e1298eb15f6bbe73cae123e
Mohamed Elafifi <mohamed2.elafifi@live.uwe.ac.uk>
2025-03-06 07:07:01 +0000

    Fixed wrong app names

--- bin/test-watch.bat
+++ bin/test-watch.bat
@@ -1,1 +1,1 @@
-docker compose -f docker-compose.yml -f docker-compose.test.yml -f docker-compose.test.watch.yml up --build --no-attach db --no-attach pgadmin
+docker compose -f docker-compose.yml -f docker-compose.test.yml -f docker-compose.test.watch.yml up --build --no-attach db --no-attach pgadmin django
