1935d6efc3305a34b7bb1898dd61f8bfcaaa8429
Mohamed Elafifi <mohamed2.elafifi@live.uwe.ac.uk>
2025-04-04 11:27:24 +0100

    Fixing Merge issue

--- InsuranceClaimsCustomer/views.py
+++ InsuranceClaimsCustomer/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import ClaimEntryForm
+from .form import ClaimEntryForm
 
 def claim_entry_view(request):
     if request.method == 'POST':
