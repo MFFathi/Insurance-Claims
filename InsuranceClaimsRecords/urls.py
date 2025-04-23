8cabbf8d7e4f937f912b07304b0ed993af614765
Faded6 <kahassan2006@gmail.com>
2025-04-23 04:35:45 +0100

    Changes in the records html page

--- InsuranceClaimsRecords/urls.py
+++ InsuranceClaimsRecords/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('records/', views.sorted_records, name='sorted_records'),
     path('records/export/', views.export_records_csv, name='export_csv'),
+    path('records/create/', views.create_record, name='create_record'),
+    path('records/<int:record_id>/edit/', views.edit_record, name='edit_record'),
+    path('records/<int:record_id>/delete/', views.delete_record, name='delete_record'),
 ]
